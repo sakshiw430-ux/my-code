ab6bfaaea2a453e4608545045b971c78e9441d5c
sakshiw430-ux <sakshiw430@gmail.com>
2026-04-16 23:33:01 +0530

    Update CodeRepresent.py

--- CodeRepresent.py
+++ CodeRepresent.py
@@ -72,3 +72,19 @@ print("Geometric Mean:", round(gm,2))
 print("Harmonic Mean:", round(hm,2))
 print("Variance:", round(variance,2))
 print("Standard Deviation:", round(std_dev,2))
+
+
+import matplotlib.pyplot as plt
+
+# Data
+x = ["1000-2000", "2000-3000", "3000-4000", "4000-5000", "5000-6000"]
+y = [5, 9, 14, 8, 4]
+
+# Bar graph
+plt.bar(x, y)
+
+plt.xlabel("Expense Range")
+plt.ylabel("Students")
+plt.title("Monthly Expense Graph")
+
+plt.show()
